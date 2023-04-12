0ed7381d298aae938cd9e00cafa1e5535fb50b28
Christian Obed Arana navas <COANelmejor@gmail.com>
2023-04-11 18:49:07 -0600

    Making a preview build of Zone Pomodoro to work

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile("index.html");
+  mainWindow.loadFile("./build/index.html");
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
